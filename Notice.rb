49d3b1d0c91b473ded389f657d79ed6ac49e7d5a
Tyler Frantz <frantz.140@osu.edu>
2021-09-30 15:46:59 -0400

    Added features to better obtain location from Safety Notice Descriptions

--- Notice.rb
+++ Notice.rb
@@ -8,16 +8,23 @@ class Notice
         noticePage = noticeMech.get link 
         
         
-        noticeDescription = noticePage.css("div[class='field__items']")[3].css('p')[1].text
-        puts "*******************"
-        puts noticeDescription
+        noticeParagraphs = noticePage.css("div[class='field__items']")[3].css('p')
+        noticeDescription = 'none'
+        noticeParagraphs.each { |i|
+        noticeDescription = i.text if (i.text.include?('a.m.') || i.text.include?('p.m.'))
+        }
+        
+        @date = noticeDescription[/\d+\/\d+\/\d+/]
+        @time = noticeDescription[/\d+(:\d+)* (a|p)\.m\./]
+        
+        @location = noticeDescription.scan(/([0-9A-Za-z]+) (Avenue|St |Ave|Street|Court|Square|Place)/)
+        puts '*****'
+        puts @date
+        puts @location[0]
+        puts @location[1]
+        puts '*****'
 
-        # still trying to figure out how to consistenly extract time and date 
 
-        #@description = noticeDescription
-        #@date = noticeDescription.split(',')[0].split(' ')[1]
-        #@time = noticeDescription.split(',')[1].split(' ')[2] + ' ' + noticeDescription.split(',')[1].split(' ')[3]
-        #@location = "TO DO"
         
     end
 
