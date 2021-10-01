7b753e7e421e30fc40925ad05a8e49705f2b4e99
Zheng Ji <tan.955@osu.edu>
2021-09-30 21:00:58 -0400

    change noticedescription to @description

--- Notice.rb
+++ Notice.rb
@@ -1,33 +1,26 @@
 # class for notice object
 class Notice
-    attr_reader :time, :date, :location, :description
-  
-    
-    def initialize(link)
-        noticeMech = Mechanize.new
-        noticePage = noticeMech.get link 
-        
-        
-        noticeParagraphs = noticePage.css("div[class='field__items']")[3].css('p')
-        noticeDescription = 'none'
-        noticeParagraphs.each { |i|
-        noticeDescription = i.text if (i.text.include?('a.m.') || i.text.include?('p.m.'))
-        }
-        
-        @date = noticeDescription[/\d+\/\d+\/\d+/]
-        @time = noticeDescription[/\d+(:\d+)* (a|p)\.m\./]
-        
-        @location = noticeDescription.scan(/([0-9A-Za-z]+) (Avenue|St |Ave|Street|Court|Square|Place)/)
-        puts '*****'
-        puts @date
-        puts @location[0]
-        puts @location[1]
-        puts '*****'
+  attr_reader :time, :date, :location, :description
 
+  def initialize(link)
+    noticeMech = Mechanize.new
+    noticePage = noticeMech.get link
 
-        
+    noticeParagraphs = noticePage.css("div[class='field__items']")[3].css('p')
+    @description = 'none'
+    noticeParagraphs.each do |i|
+      @description = i.text if i.text.include?('a.m.') || i.text.include?('p.m.')
     end
 
-    
-  
+    @date = @description[%r{\d+/\d+/\d+}]
+    @time = @description[/\d+(:\d+)* (a|p)\.m\./]
+    @location = @description.scan(/([0-9A-Za-z]+) (Avenue|St |Ave|Street|Court|Square|Place)/)
+
+    # puts '*****'
+    # puts @date
+    # puts @time
+    # puts @location[0]
+    # puts @location[1]
+    # puts '*****'
   end
+end
