4ed7ee8fbdd4b4de379f7fcbd511d396f0fa5a77
Tyler Frantz <frantz.140@osu.edu>
2021-10-02 13:48:15 -0400

    Added functionality to notice class to better parse for location of notices

--- notice.rb
+++ notice.rb
@@ -13,13 +13,44 @@ class Notice
 
     @date = @description[%r{\d+/\d+/\d+}]
     @time = @description[/\d+(:\d+)* (a|p)\.m\./]
-    @location = @description.scan(/([0-9A-Za-z]+) (Avenue|St |Ave|Street|Court|Square|Place)/)
+    @location = @description.scan(/([0-9A-Za-z]+) (Avenue|St\.|St |Ave\.*|Street|Court|Square|Place)/)
 
-    # puts '*****'
-    # puts @date
-    # puts @time
-    # puts @location[0]
-    # puts @location[1]
-    # puts '*****'
+    # if first attempt cannot find any street names in traditional format, look for paired streets
+    if @location.length == 0
+      streets = @description[/[a-zA-Z0-9]+ and [a-zA-Z0-9]+ (avenues|streets)/]
+      if !streets.nil?
+        streets = streets.split ' '
+        @location[0] = streets[0]
+        @location[1] = streets[2]
+      end
+    end
+    
+    # if second attempt cannot find any streets in paired format, look for any streets on/near campus listed
+    if @location.length == 0
+      puts 'third'
+      streets = ['8th', '9th', '10th', '11th', 'Chittenden', '12th', '13th', '14th', '15th', '16th', '17th', '18th', 'Woodruff', 'Frambes',
+      'Lane', 'Norwich', 'Pearl', 'Northwood', 'Oakland', 'Patterson', 'Maynard', 'Blake', 'Neil', 'High', 'Indianola', 'Summit', '4th']
+      streets.each { |i| @location << i if @description.include? i}
+    end
+
+    # set location to be two streets to eliminate duplicates
+    @location = @location[0,2]
+
+     #puts '*****'
+     #puts @location.length
+     #puts @date
+     #puts @time
+     #puts @location[0]
+     #puts @location[1]
+     #puts '*****'
+
+  # retrieve year from the date
+  def retrieve_year
+    temp = @date.to_s.split '/'
+    # @year = 0 if temp[2] is blank/no date
+    @year = temp[2].to_i
+    # prevent addition of 2000 if 0
+    @year += 2000 if !@year.zero? & (@year < 2000)
+  end
   end
 end
