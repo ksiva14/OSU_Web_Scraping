3ebf9a9abf8e75be954ee38268315e036c2fa07a
Zheng Ji <tan.955@osu.edu>
2021-10-02 15:27:51 -0400

    moved location to it's own function

--- notice.rb
+++ notice.rb
@@ -14,28 +14,7 @@ class Notice
     retrieve_date
     retrieve_year
     retrieve_time
-    @location = @description.scan(/([0-9A-Za-z]+) (Avenue|St\.|St |Ave\.*|Street|Court|Square|Place)/)
-
-    # if first attempt cannot find any street names in traditional format, look for paired streets
-    if @location.length == 0
-      streets = @description[/[a-zA-Z0-9]+ and [a-zA-Z0-9]+ (avenues|streets)/]
-      unless streets.nil?
-        streets = streets.split ' '
-        @location[0] = streets[0]
-        @location[1] = streets[2]
-      end
-    end
-
-    # if second attempt cannot find any streets in paired format, look for any streets on/near campus listed
-    if @location.length == 0
-      # puts 'third'
-      streets = %w[8th 9th 10th 11th Chittenden 12th 13th 14th 15th 16th 17th 18th Woodruff Frambes
-                   Lane Norwich Pearl Northwood Oakland Patterson Maynard Blake Neil High Indianola Summit 4th]
-      streets.each { |i| @location << i if @description.include? i }
-    end
-
-    # set location to be two streets to eliminate duplicates
-    @location = @location[0, 2]
+    retrieve_location
 
     # puts '*****'
     # puts "#{@date} --- #{@time}"
@@ -67,5 +46,31 @@ class Notice
   def retrieve_time
     @time = @description[/\d+(:\d+)* (a|p)\.m\./]
     @time = nil if @time.to_s.strip.empty?
+  end
+
+  # retrieve the location of crime
+  def retrieve_location
+    @location = @description.scan(/([0-9A-Za-z]+) (Avenue|St\.|St |Ave\.*|Street|Court|Square|Place)/)
+
+    # if first attempt cannot find any street names in traditional format, look for paired streets
+    if @location.length == 0
+      streets = @description[/[a-zA-Z0-9]+ and [a-zA-Z0-9]+ (avenues|streets)/]
+      unless streets.nil?
+        streets = streets.split ' '
+        @location[0] = streets[0]
+        @location[1] = streets[2]
+      end
+    end
+
+    # if second attempt cannot find any streets in paired format, look for any streets on/near campus listed
+    if @location.length == 0
+      # puts 'third'
+      streets = %w[8th 9th 10th 11th Chittenden 12th 13th 14th 15th 16th 17th 18th Woodruff Frambes
+                   Lane Norwich Pearl Northwood Oakland Patterson Maynard Blake Neil High Indianola Summit 4th]
+      streets.each { |i| @location << i if @description.include? i }
+    end
+
+    # set location to be two streets to eliminate duplicates
+    @location = @location[0, 2]
   end
 end
