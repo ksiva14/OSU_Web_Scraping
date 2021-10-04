de67c64f7ae1bc18c37e14380fe0ba448a9e9427
Zheng Ji <tan.955@osu.edu>
2021-10-04 14:42:29 -0400

    created test folder. created file for each .rb. added sample test to each test file.

---
+++ test/test_page.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require '../lib/page'
+require 'ruby2d'
+
+# Test fixture for Player Class
+class TestPlayer < Minitest::Test
+  # Has name attribute
+  def sample
+    # assert_respond_to Player.new('player 1', 50, 100), :name
+    # assert_equal 1, @player.score
+  end
+end
